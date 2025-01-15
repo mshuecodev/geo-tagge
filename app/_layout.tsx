f90970f8776b0d3fcef2e3239a7f293ff9a736cd
misna04 <misnasyari@gmail.com>
2025-01-15 08:47:58 +0800

    simple navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,71 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native"
-import { useFonts } from "expo-font"
-import * as SplashScreen from "expo-splash-screen"
-import { useEffect } from "react"
-import "react-native-reanimated"
-import { Tabs } from "expo-router"
-import { Ionicons } from "@expo/vector-icons"
-
-import { useColorScheme } from "@/hooks/useColorScheme"
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
+import { Stack } from "expo-router"
 
 export default function RootLayout() {
-	const colorScheme = useColorScheme()
-	const [loaded] = useFonts({
-		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
-	})
-
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync()
-		}
-	}, [loaded])
-
-	if (!loaded) {
-		return null
-	}
-
 	return (
-		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<Tabs
-				initialRouteName="home"
-				screenOptions={{
-					tabBarActiveTintColor: "tomato",
-					tabBarInactiveTintColor: "gray",
-					headerShown: false
-				}}
-			>
-				<Tabs.Screen
-					name="home"
-					options={{
-						title: "Home",
-						headerShown: false,
-						tabBarIcon: ({ color }) => (
-							<Ionicons
-								size={28}
-								name="home"
-								color={color}
-							/>
-						)
-					}}
-				/>
-				<Tabs.Screen
-					name="profile"
-					options={{
-						title: "Profile",
-						headerShown: false,
-						tabBarIcon: ({ color }) => (
-							<Ionicons
-								size={28}
-								name="person"
-								color={color}
-							/>
-						)
-					}}
-				/>
-			</Tabs>
-		</ThemeProvider>
+		<Stack>
+			<Stack.Screen
+				name="index"
+				options={{ title: "Home" }}
+			/>
+			<Stack.Screen
+				name="about"
+				options={{ title: "About" }}
+			/>
+		</Stack>
 	)
 }
