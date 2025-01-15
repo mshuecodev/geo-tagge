3b611a6f36620e8d6e6d2b72b20cc42a085c16b0
misna04 <misnasyari@gmail.com>
2025-01-15 08:58:08 +0800

    tab navigatin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,13 +4,18 @@ export default function RootLayout() {
 	return (
 		<Stack>
 			<Stack.Screen
+				name="(tabs)"
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen name="+not-found" />
+			{/* <Stack.Screen
 				name="index"
 				options={{ title: "Home" }}
 			/>
 			<Stack.Screen
 				name="about"
 				options={{ title: "About" }}
-			/>
+			/> */}
 		</Stack>
 	)
 }
